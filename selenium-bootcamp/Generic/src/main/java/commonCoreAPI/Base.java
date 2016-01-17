151f19cfb8ed92ebc7c6f1a0034bcc87f60f9402
rashed1 <rashed_023@yahoo.com>
2016-01-17 11:05:55 -0500

    Initial commit

---
+++ selenium-bootcamp/Generic/src/main/java/commonCoreAPI/Base.java
@@ -0,0 +1,7 @@
+package commonCoreAPI;
+
+/**
+ * Created by Rashed on 1/17/2016.
+ */
+public class Base {
+}
---
+++ selenium-bootcamp/Generic/src/main/java/util/ConnectDB.java
@@ -0,0 +1,7 @@
+package util;
+
+/**
+ * Created by Rashed on 1/17/2016.
+ */
+public class ConnectDB {
+}
